506a617777aa7556000f83c795a55ca7c2e2585f
Prajwol Shrestha <132246307+prajwolstha@users.noreply.github.com>
2024-02-01 22:00:34 +0545

    updated

--- oop_practise_set.java
+++ oop_practise_set.java
@@ -1,5 +1,35 @@
 package javaPrajwol;
+class Help{
+	int salary;
+	String name;
+	
+	public int getSalary() {
+		return salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+}
 
 public class oop_practise_set {
 
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Help prajwol= new Help();
+		prajwol.setName("codeWithPrajwol");
+		prajwol.salary=55000;
+		System.out.println(prajwol.getName());
+		
+		System.out.println(prajwol.getSalary());
+		
+	}
 }
