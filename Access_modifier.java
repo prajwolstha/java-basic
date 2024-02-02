881b9023fb319483f9aa81e05f0ce4e299e9fdad
Prajwol Shrestha <132246307+prajwolstha@users.noreply.github.com>
2024-02-02 19:48:18 +0545

    Code Updated

---
+++ Access_modifier.java
@@ -0,0 +1,16 @@
+package javaPrajwol;
+class myEmployee{
+	
+	
+	int id;
+    String name;
+}
+public class Access_modifier {
+public static void main(String[] args) {
+	
+	
+	myEmployee prajwol= new myEmployee();
+	prajwol.id=45;
+	prajwol.name="prajwol";
+}
+}
---
+++ gitPush.bat
@@ -0,0 +1,15 @@
+@echo off
+
+git init
+
+git status
+
+git add .
+
+git status
+
+git commit -m "Code Updated"
+
+git push
+
+exit
