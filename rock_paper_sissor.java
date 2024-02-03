3846ab7e79efd44ea7fc5b0d7d5bdddd6400797c
Prajwol Shrestha <132246307+prajwolstha@users.noreply.github.com>
2024-02-03 18:17:21 +0545

    new file added

---
+++ rock_paper_sissor.java
@@ -0,0 +1,60 @@
+package javaPrajwol;
+import java.util.Scanner;
+import java.util.Random;
+public class rock_paper_sissor {
+public static void main(String[] args) {
+	
+
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter yout number between(1-3");
+	int number=sc.nextInt();
+	Random random = new Random();
+	int randomN=random.nextInt(3);
+
+	switch (number) {
+	case 1:
+		System.out.println("rock");
+		break;
+	case 2:
+		System.out.println("paper");
+		break;
+	case 3:
+		System.out.println("scissor");
+		break;
+		
+		default:
+			System.out.println("Please enter valid number");
+			System.exit(0);
+	}
+	
+	switch (randomN) {
+	case 1:
+		System.out.println("rock");
+		break;
+	case 2:
+		System.out.println("paper");
+		break;
+	case 3:
+		System.out.println("scissor");
+		break;
+		
+	}
+	
+	if(number==randomN) {
+		System.out.println("Draw");
+	}
+		else if(number==1 && randomN==2|| number==2 && randomN==3 || number==3 && randomN==1) {
+			System.out.println("computer wins");
+		}
+			else{
+				System.out.println("You win");
+			}
+	}
+	
+	
+	
+	
+	
+	
+}
+
