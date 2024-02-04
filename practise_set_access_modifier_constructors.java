ecd2ef375addbd9cccaf54546ba389ed8b4b9997
Prajwol Shrestha <132246307+prajwolstha@users.noreply.github.com>
2024-02-04 22:04:12 +0545

    Code Updated

---
+++ practise_set_access_modifier_constructors.java
@@ -0,0 +1,51 @@
+package javaPrajwol;
+
+class cylinder{
+	
+	private int radius;
+	private int height;
+	
+	public int getRadius() {
+		return radius;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	
+	public int surfaceArea() {
+		
+		return 2*22/7*radius*height+2*22/7*radius;
+		
+	}
+	public int volume() {
+		return 22/7*radius*radius*height;
+	}
+	
+}
+
+
+
+
+public class practise_set_access_modifier_constructors {
+public static void main(String[] args) {
+	cylinder math=new cylinder();
+    math.setRadius(3);
+    math.setHeight(10);
+    System.out.println(math.getRadius());
+    System.out.println(math.getHeight());
+    System.out.println(math.surfaceArea());
+    
+    System.out.println(math.volume());
+	
+	
+    
+	
+}
+}
